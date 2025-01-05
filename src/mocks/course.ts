c965f2608b3f7f10b1eff6eabb9852170e3dc880
Himanshu Chandel <himanshuchandel107@gmail.com>
2025-01-05 13:35:48 +0000

    added mock for course view data

---
+++ src/mocks/course.ts
@@ -0,0 +1,22 @@
+const CourseViewData = [
+  {
+    id: '123',
+    title: 'Chapter 1',
+    subtitle: 'Chapter 1 Subtitle',
+    sections: [
+      { id: '123', title: 'Chapter1Section1 Title', subTitle: 'Subtitle' },
+      { id: '121', title: 'Chapter1Section2 Title', subTitle: 'Subtitle' },
+    ],
+  },
+  {
+    id: '191',
+    title: 'Chapter 2',
+    subtitle: 'Chapter 2 Subtitle',
+    sections: [
+      { id: '123', title: 'Chapter2Section1 Title', subTitle: 'Subtitle' },
+      { id: '121', title: 'Chapter2Section2 Title', subTitle: 'Subtitle' },
+    ],
+  },
+];
+
+export { CourseViewData };
