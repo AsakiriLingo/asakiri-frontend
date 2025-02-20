4f2b016b287fb87c4b15578c38ca5badf5bdf9c4
Mayank <mayankgirdhar0001@gmail.com>
2025-02-20 20:09:21 +0530

    Create Profile if not having

--- src/features/auth/providers/auth-provider.tsx
+++ src/features/auth/providers/auth-provider.tsx
@@ -1,3 +1,4 @@
+import { User } from '@supabase/supabase-js';
 import React, { useEffect, useState } from 'react';
 
 import { useAuthStore } from '../stores/auth-store';
@@ -13,10 +14,31 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   const { setAuthState } = useAuthStore();
 
+  const createOrUpdateProfile = async (user: User) => {
+    try {
+      const { data: existingProfile } = await supabase
+        .from('profiles')
+        .select('id, name, avatar_url')
+        .eq('id', user.id)
+        .maybeSingle();
+
+      if (!existingProfile) {
+        const { error: insertError } = await supabase.from('profiles').insert({
+          id: user.id,
+          name: user.user_metadata?.full_name || user.email?.split('@')[0],
+          updated_at: new Date().toISOString(),
+        });
+
+        if (insertError) throw insertError;
+      }
+    } catch (error) {
+      console.error('Error upserting profile:', error);
+    }
+  };
+
   useEffect(() => {
     const initializeAuth = async () => {
       try {
-        // Get initial session
         const {
           data: { session },
           error,
@@ -25,24 +47,24 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 
         if (session) {
           const { user } = session;
-          const {
-            data: { avatar_url },
-          } = await supabase
+
+          await createOrUpdateProfile(user);
+
+          const { data: profile, error: profileError } = await supabase
             .from('profiles')
-            .select(
-              `
-          *
-        `
-            )
+            .select('id, name, avatar_url')
             .eq('id', user.id)
             .single();
+
+          if (profileError) throw profileError;
+
           setAuthState({
             isAuthenticated: true,
             user: {
               id: user.id,
               email: user.email || undefined,
-              name: user.user_metadata?.name as string | undefined,
-              avatar: avatar_url as string | undefined,
+              name: profile.name || user.user_metadata?.full_name,
+              avatar: profile.avatar_url || '',
             },
             accessToken: session.access_token,
           });
@@ -62,7 +84,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
       <div>
         <LoadingSpinner />
       </div>
-    ); // Replace with your loading component
+    );
   }
 
   return <>{children}</>;
