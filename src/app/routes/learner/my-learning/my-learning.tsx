db3aaf97ea102ba4aaeee6efad84a4628246cadb
Mayank <mayankgirdhar0001@gmail.com>
2025-02-21 18:22:11 +0530

    My learning

--- src/app/routes/learner/my-learning/my-learning.tsx
+++ src/app/routes/learner/my-learning/my-learning.tsx
@@ -41,6 +41,7 @@ const CourseLearningRoute: React.FC = () => {
               author_subtitle={course.author_subtitle}
               author_avatar_url={course.author_avatar_url}
               enrolled_students={course.enrolled_students}
+              showTotalEnrolled={true}
               created_at={course.author_id}
               category={''}
               link={`/course/viewer/${course.id}`}
